491dc7254a2e7f7d70d19931f0d2cd926edb588f
Abhishek Deo <Deo.Abhishek4@gmail.com>
2022-05-23 16:23:55 +0900

    Work stuff

--- models/asis.py
+++ models/asis.py
@@ -3,62 +3,85 @@ from diagrams.custom import Custom
 
 
 GRAPH_ATTRIBUTES = {
-    "fontsize": "45",
-    "bgcolor": "transparent"
+    "layout":"dot",
+    "compound":"true",
+    "splines":"spline",
 }
 
+SQ_ICON_LOCATION = "./images/square.png"
 DB_ICON_LOCATION = "./images/dbicon.png"
 
-DB_ICON = Custom("ICON NAME", DB_ICON_LOCATION)
+# DB_ICON = Custom("ICON NAME", DB_ICON_LOCATION)
 
 
 def model():
     with Diagram(
-        "Web Service",
+        "DWH/ETL AsIsモデル",
         show=False,
         graph_attr=GRAPH_ATTRIBUTES,
         outformat="pdf",
-        filename="asis"
+        filename="test"
     ):
-        # Data Source
         with Cluster("Data Source"):
-            # Mainframe Block
             with Cluster("Mainframe"):
-                pass
+                with Cluster("1"):
+                    mf_contents = [
+                        Custom("証券", DB_ICON_LOCATION),
+                        Custom("顧客", DB_ICON_LOCATION),
+                        Custom(". . .", DB_ICON_LOCATION)
+                    ]
 
-            # Open Platform Block
             with Cluster("Open Platform"):
-                pass
+                with Cluster("2"):
+                    op_contents = [
+                        Custom("AANET", DB_ICON_LOCATION),
+                        Custom("CANET", DB_ICON_LOCATION),
+                        Custom("OHP", DB_ICON_LOCATION),
+                        Custom(". . .", DB_ICON_LOCATION)
+                    ]
 
-            # Client Block
-            with Cluster("Client"):
-                with Cluster("Client"):
-                    pass
-                pass
+            # with Cluster("Client"):
+            #     with Cluster("ユーザーデータ"):
+            #         # Client[User Data] Block
+            #         ud = Custom("ユーザー作成データ", DB_ICON_LOCATION)
 
-            # Cloud Platform Block
-            with Cluster("Cloud Platform"):
-                with Cluster("SFDC"):
-                    pass
-                pass
+            # with Cluster("Cloud Platform"):
+            #     with Cluster("SFDC"):
+            #         cp_contents = [
+            #             Custom("SLM", DB_ICON_LOCATION),
+            #             Custom(". . .", DB_ICON_LOCATION)
+            #         ]
 
-            pass
-
-        # Data Integration
         with Cluster("Data Integration"):
-            pass
+            # ETL/PC
+            etl_pc_sq = Custom("ETL/PowerCenter", SQ_ICON_LOCATION)
 
-        # Data Storage & Aggregation
-        with Cluster("Data Storage and Aggregation"):
-            pass
+            # acc
+            acc_sq = Custom(
+                "Access Navigator WEB\n ※データ投入機能TERADATA",
+                SQ_ICON_LOCATION
+            )
 
-        # Data Access & Delivery
-        with Cluster("Data Access & Delivery"):
-            pass
+            # TDP
+            tdp_sq = Custom(
+                "TERADATA TDP\n （チャネル接続）",
+                SQ_ICON_LOCATION
+            )
 
-        # Data Consumers
-        with Cluster("Data Consumers"):
-            pass
+            # DataSpider
+            # dsp_sq = Custom("DataSpider", SQ_ICON_LOCATION)
+
+        Edge()
+
+        mf_contents[1] >> Edge(ltail = "cluster_1") >> etl_pc_sq
+        mf_contents[2] >> Edge(ltail = "cluster_1") >> tdp_sq
+
+        op_contents[1] >> Edge(ltail = "cluster_2") >> etl_pc_sq
+
+        # ud >> Edge(style="dotted") >> acc_sq
+        # ud >> Edge(style="dotted") >> dsp_sq
+
+        # cp_contents >> etl_pc_sq
 
 
 if "__main__" == __name__:
